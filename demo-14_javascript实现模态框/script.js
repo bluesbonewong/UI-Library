50553287e07c2adcd42141c30e0a3ba5cb6fdee3
bluesbonew <bluesbonewong@gmail.com>
2017-10-17 10:21:18 +0800

    addfile

---
+++ demo-14_javascript实现模态框/script.js
@@ -0,0 +1,24 @@
+var btnNode = document.querySelector('button')
+var frameBtnNodes = document.querySelectorAll('#frame .bottom a')
+var frameNode = document.querySelector('#frame')
+var bodyNode = document.querySelector('body')
+
+function closeFrame() {
+	frameNode.className = ''
+	bodyNode.className = ''
+}
+
+btnNode.addEventListener('click', function (e) {
+	frameNode.className = 'display'
+	bodyNode.className = 'grey'
+	e.stopPropagation()
+})
+
+frameBtnNodes[0].addEventListener('click', closeFrame)
+frameBtnNodes[1].addEventListener('click', closeFrame)
+bodyNode.addEventListener('click', function (e) {
+	if (e.target.tagName.toLowerCase() === 'body') {
+		frameNode.className = ''
+		bodyNode.className = ''
+	}
+})
